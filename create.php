9eed3324b1b6a2a081bd78a39184895e81973cff
unknown <ahmadrayhan0122@gmail.com>
2023-08-31 20:27:49 +0700

    kedua

---
+++ create.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+ <html lang="en">
+
+
+<head>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Create siswa</title>
+
+</head>
+
+<body>
+
+<form action="act_create.php" method="get">
+<label for="">nama: </label>
+<input type="text" name="nama" id=" ">
+<br><br>
+
+<label for="">jenis kelamin: </label>
+<input type="radio" name="jenis kelamin" value="laki-laki">laki-laki
+<input type="radio" name="jenis kelamin" value="perempuan">perempuan
+<br><br>
+
+<label for="">No Telepon: </label>
+<input type="tel" name="no_tlp" id="">
+<br><br>
+<button type="submit">Tambah Siswa</button>
+</form>
+</body>
+ </html>
