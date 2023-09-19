06848ef6b19c4e10ffa81af64861686b06ffd3de
Ttwone <140868531+Ttwone@users.noreply.github.com>
2023-09-19 11:23:15 +0700

    Update koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet"type="text/css"href="styles.css">
+
+</head>
+<body>
+    <section id="gallery" class="gallery">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-18 text-center">
+                <span>memori kisah kami</span>
+                <h2></h2>
+                <p>lorem ipsum dolor sit consectetur adipisicing elit, fuge, itaque?</p>
+            </div>
+        </div>
+    </section>
+    
+    <section id="rsvp" class="rsvp">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-18 text-center">
+                <h2>konfirmasi kehadiran</h2>
+                <p>isi form di bawah ini untuk mengisi konfirmasi kehadiran.</p>
+            </div>
+        </div>
+    
+    
+        </div>
+    </section>
+</body>
+</html>
